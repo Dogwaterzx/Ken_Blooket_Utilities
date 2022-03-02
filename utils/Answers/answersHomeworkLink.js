8712eefc69762b2fe9642e8e923e78ea3741ecb9
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-03-02 14:56:53 -0500

    Gives you answers for homework
    Gives you answers for homework

---
+++ utils/Answers/answersHomeworkLink.js
@@ -0,0 +1,20 @@
+(async () => {
+
+    alert("Made by: glizzz_y#0777");
+    const hwid = prompt('Please input the HWID');
+
+    const GetAnswers = await fetch(`https://api.blooket.com/api/homeworks/byid?id=${hwid}`, {
+        headers: {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/json;charset=UTF-8",
+        },
+        method: 'GET'
+    });
+    const Answers = await GetAnswers.json();
+
+    Answers.questions.forEach(question => {
+        console.log(`Q: ${question.text} A: ${question.correctAnswers.join(', ')}`); 
+    });
+
+})();
