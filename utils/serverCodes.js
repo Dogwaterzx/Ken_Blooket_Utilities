cca15cc30c4f57165fabbbf5d01825d1d99790e9
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-03-03 16:33:40 -0500

    Create serverCodes.js

---
+++ utils/serverCodes.js
@@ -0,0 +1,57 @@
+function serverCodes() {
+    return [
+        {
+            code: 2026,
+        },
+        {
+            code: 2028,
+        },
+        {
+            code: 2025,
+        },
+        {
+            code: 2037,
+        },
+        {
+            code: 2023,
+        },
+        {
+            code: 2020,
+        },
+        {
+            code: 2031,
+        },
+        {
+            code: 2022,
+        },
+        {
+            code: 2027,
+        },
+        {
+            code: 2034,
+        },
+        {
+            code: 2024,
+        },
+        {
+            code: 2030,
+        },
+        {
+            code: 2036,
+        },
+        {
+            code: 2033,
+        },
+        {
+            code: 2032,
+        },
+        {
+            code: 2021,
+        },
+        {
+            code: 2035,
+        },
+    ]
+};
+
+module.exports = serverCodes
