78d192fd6efccf6c372173164087faea9ad98389
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-03-13 10:49:22 -0400

    Create getRandomString.js

---
+++ utils/getRandomString.js
@@ -0,0 +1,3 @@
+/* thanks to https://github.com/charleywright my horrible coding now looks good. */
+const tips = ["Tip0","Tip1","Tip2","Tip3","Tip4","Tip5","Tip6","Tip7","Tip8","Tip9","Tip10"];
+const getTip = () => tips[Math.floor(Math.random() * 11)];
