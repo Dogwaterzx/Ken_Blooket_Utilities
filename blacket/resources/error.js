9dc7f3b373af49f4cb0e3ab226e18d5ddf362df9
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-03-16 15:40:06 -0400

    Update error.js

--- blacket/resources/error.js
+++ blacket/resources/error.js
@@ -1,6 +1,6 @@
 function error() {
     if (confirm("an error has ocured, would you like to report this on github?")) {
-        window.open("https://github.com/ZasticBradyn/blacket-hacks/issues")
+        window.open("https://github.com/Dogwaterzx/Ken_Blooket_Utilities/issues")
     }
     console.error("an error has occured")
 }
